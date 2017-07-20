12c8e209f9740b2affa70c6024abaa081f84fd65
Mark Goho <markgoho@gmail.com>
2017-07-20 17:00:51 -0400

    Adding new doula

--- webp.js
+++ webp.js
@@ -2,12 +2,14 @@ const imagemin = require('imagemin');
 const webp = require('imagemin-webp');
 
 // webp config
-const outputFolder = './img/headshots/thumbs';
-const JPEGImages = './img/headshots/thumbs/*.jpg';
+const outputFolder = './images-to-convert';
+const JPEGImages = './images-to-convert/*.jpg';
 
 imagemin([JPEGImages], outputFolder, {
-	plugins: [webp({
-    quality: 65,
-    method: 6
-  })]
+  plugins: [
+    webp({
+      quality: 65,
+      method: 6
+    })
+  ]
 });
