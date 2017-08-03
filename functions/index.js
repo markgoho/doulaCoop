8e44af1827ff0ff5a6804343ab828c3037ad9387
Mark Goho <markgoho@gmail.com>
2017-08-03 11:12:38 -0400

    Added express route to members-only

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,10 @@
 const functions = require('firebase-functions');
+const express = require('express');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.get('/members-only', (request, response) => {
+  response.send(`You're here at the members-only section. Coming soon!`);
+});
+
+exports.app = functions.https.onRequest(app);
