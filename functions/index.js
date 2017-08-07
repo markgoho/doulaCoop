dd98edb3a0c5f54285e3cfa60301843e9d9116e3
Mark Goho <markgoho@gmail.com>
2017-08-06 22:24:55 -0400

    Moving index to express and pug

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,11 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.get('/', (request, response) => {
+  response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+  response.render(`index`, { title: 'index' });
+});
+
 app.get('/members-only', (request, response) => {
   response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
   response.render(`members-only`, { title: 'monthly-meetings' });
