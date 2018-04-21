3c7b6d3d96310818d661adca366507cf1080a8d6
David Johnson <ultimadj@gmail.com>
2018-04-20 21:42:35 -0400

    Extended pre-receive hook with file and authorized user restrictions

--- .github/pre-receive.sh
+++ .github/pre-receive.sh
@@ -1,16 +1,60 @@
 #!/usr/bin/env bash
 # https://github.com/github/platform-samples/blob/master/pre-receive-hooks/block_unknown_pushers.sh
 
+
+
+#!/usr/bin/env bash
+
 #
-# Pre-receive hook that will block any pushes / repository modifications
-# not performed by a user in the list
+# Pre-receive hook that will block any new commits that contain files ending
+# with .gz, .zip or .tgz
 #
 # More details on pre-receive hooks and how to apply them can be found on
 # https://help.github.com/enterprise/admin/guides/developer-workflow/managing-pre-receive-hooks-on-the-github-enterprise-appliance/
 #
 
-case $GITHUB_USER_LOGIN in
-  markgoho) echo "User $GITHUB_USER_LOGIN is allowed to push";;
-  *) echo "User $GITHUB_USER_LOGIN is not in the list of authorized pushers"
-     exit 1;;
-esac
+zero_commit="0000000000000000000000000000000000000000"
+
+# Do not traverse over commits that are already in the repository
+# (e.g. in a different branch)
+# This prevents funny errors if pre-receive hooks got enabled after some
+# commits got already in and then somebody tries to create a new branch
+# If this is unwanted behavior, just set the variable to empty
+excludeExisting="--not --all"
+
+while read oldrev newrev refname; do
+  # echo "payload"
+  echo $refname $oldrev $newrev
+
+  # branch or tag get deleted
+  if [ "$newrev" = "$zero_commit" ]; then
+    continue
+  fi
+
+  # Check for new branch or tag
+  if [ "$oldrev" = "$zero_commit" ]; then
+    span=`git rev-list $newrev $excludeExisting`
+  else
+    span=`git rev-list $oldrev..$newrev $excludeExisting`
+  fi
+
+  for COMMIT in $span;
+  do
+    for FILE  in `git log -1 --name-only --pretty=format:'' $COMMIT`;
+    do
+      case $FILE in
+      *config.yml|*pre-receive.sh )
+        echo "WARNING: This changeset includes files that affect the build or repository restrictions"
+
+        case $GITHUB_USER_LOGIN in
+            markgoho) echo "User $GITHUB_USER_LOGIN is allowed to push";;
+            *) echo "User $GITHUB_USER_LOGIN is not in the list of authorized pushers"
+                exit 1;;
+        esac
+        
+        ;;
+      esac
+    done
+  done
+done
+exit 0
